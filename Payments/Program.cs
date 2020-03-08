cbc92297798cc12523cc082e0356244d66ef4961
HMansur <hmsmansur@yahoo.com>
2020-03-08 07:49:57 +0000

    Update Program.cs

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -16,17 +16,33 @@ namespace PortfolioPayments
             //string path = Console.ReadLine();
             string path = @"C:\Users\hasan\Downloads\payments.csv";
             PaymentCollection paymentCollection;
-
-            if (File.Exists(path)) File.Delete(path);
-            using (WebClient webClient = new WebClient())
+            int payCount = 0;
+            
+            if (File.Exists(path))
             {
-                var rawJSON = webClient.DownloadString(@"https://focus.interview/api/v1.0/payments/{100,110,155}");
-                paymentCollection = JsonConvert.DeserializeObject<PaymentCollection>(rawJSON);
+                File.Delete(path);
             }
 
-            WriteToCSVFile(paymentCollection, path);
-            Console.WriteLine(paymentCollection.Payments.Count + " Payments downloaded...");
-            Console.ReadLine();
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    string rawJSON = webClient.DownloadString(@"https://focus.interview/api/v1.0/payments/{100,110,155}");
+                    paymentCollection = JsonConvert.DeserializeObject<PaymentCollection>(rawJSON);
+                    payCount = paymentCollection.Payments.Count();
+                    WriteToCSVFile(paymentCollection, path);
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("There was an error in the URL: " + ex.Message.ToString() + "");
+                    Console.ReadLine();
+                }
+            }
+
+            if (payCount>0)
+            {
+                Console.WriteLine("" + payCount + " Payments downloaded...");
+                Console.ReadLine();
+            }
         }
         private static void WriteToCSVFile(PaymentCollection list, string path)
         {
